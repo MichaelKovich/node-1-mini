b85ebf84a3b299ef485b06d0a33c68cd7ddcaa90
Michael Kovich <kovich@michaelkovich.com>
2018-02-28 16:13:19 -0600

    Initial commit. Project complete.

--- server/index.js
+++ server/index.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bookCtrl = require(`${__dirname}/controllers/books_controller.js`);
 
 const app = express();
 
-app.use( bodyParser.json() );
+app.use(bodyParser.json());
+app.use(express.static(`${__dirname}/../public/build`));
 
-app.listen((process.env.PORT || 3000), () => { console.log(`Server listening on port ${port}`); } );
+const baseURL = "/api/books";
+app.post(baseURL, bookCtrl.create);
+app.get(baseURL, bookCtrl.read);
+app.put(`${baseURL}/:id`, bookCtrl.update);
+app.delete(`${baseURL}/:id`, bookCtrl.delete);
+
+const port = 3000;
+app.listen(port, () => {console.log(`Server listening on port ${port}`);});
