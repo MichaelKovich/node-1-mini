c3d72a18e8b7ef1e27918c154e316b0c6e62814f
Michael Kovich <kovich@michaelkovich.com>
2018-02-28 14:54:27 -0600

    Port 3000.

--- server/index.js
+++ server/index.js
@@ -5,4 +5,4 @@ const app = express();
 
 app.use( bodyParser.json() );
 
-app.listen((process.env.PORT || 3000), () => { console.log(`Server listening on port ${port}`); } );
+app.listen(3000, () => { console.log(`Server listening on port ${port}`); } );
